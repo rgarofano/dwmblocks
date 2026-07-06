da6bd896880d21c461739fa32042a73675c7b1ac
Ryan Garofano <ryanjgarofano@gmail.com>
2026-07-06 12:48:15 -0700

    Change script naming convention

--- blocks.def.h
+++ blocks.def.h
@@ -1,8 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",             "volume",	                0,		        10},
-	{"",             "clock",	               10,		         0},
+	{"",          "block-volume",	                0,		        10},
+	{"",          "block-datetime",	                1,		         0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
