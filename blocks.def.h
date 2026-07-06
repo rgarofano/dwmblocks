52cec6e2bc228e4291eb29e5a2a181137e463e71
Ryan Garofano <ryanjgarofano@gmail.com>
2026-07-06 16:27:01 -0700

    Fix icon spacing

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-        {" ",        "block-memory",                  10,                       0},
+        {"  ",       "block-memory",                  10,                       0},
 	{"",          "block-volume",	                0,		        10},
-	{"",          "block-datetime",	                1,		         0},
+	{"  ",       "block-datetime",	                1,		         0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
