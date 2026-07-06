8dbeb98bcbd11911d62871b8a1cad05251220922
Ryan Garofano <ryanjgarofano@gmail.com>
2026-07-06 15:35:05 -0700

    Add memory usage

--- blocks.def.h
+++ blocks.def.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+        {" ",        "block-memory",                  10,                       0},
 	{"",          "block-volume",	                0,		        10},
 	{"",          "block-datetime",	                1,		         0},
 };
