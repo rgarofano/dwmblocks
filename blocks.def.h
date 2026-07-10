8fbdbd64e63ef827591518935c90a160ac6dffe2
Ryan Garofano <ryanjgarofano@gmail.com>
2026-07-10 16:33:57 -0700

    Add block for battery (commented out by default)

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,11 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-        {" ",        "block-memory",                  10,                       0},
-	{"",          "block-volume",	                0,		        10},
-	{"  ",       "block-datetime",	                1,		         0},
+// Icon, Command, Update Interval, Update Signal
+    {" ", "block-memory", 10, 0},
+//  {"", "block-battery", 60, 0},
+    {"", "block-volume", 0, 10},
+    {"  ", "block-datetime", 1, 0},
 };
 
-//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+// Sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
