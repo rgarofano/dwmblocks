e425ba73b8c0564fa2e8999bfee5fe2c88d95a7d
Ryan Garofano <ryanjgarofano@gmail.com>
2026-07-12 00:24:27 -0700

    Add cpu usage to status bar

--- blocks.def.h
+++ blocks.def.h
@@ -1,6 +1,7 @@
 static const Block blocks[] = {
 // Icon, Command, Update Interval, Update Signal
     {" ", "status-network", 0, 11},
+    {"", "status-cpu", 5, 0},
     {"", "status-memory", 10, 0},
 //  {"", "status-battery", 60, 0},
     {"", "status-volume", 0, 10},
